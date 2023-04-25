b64a38c1b14abed46ca34563c2d179a6a40f3ce8
breanne-oo <oot@oregonstate.edu>
2023-04-24 18:20:50 -0700

    first commit

---
+++ PA1/agent.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from board import Board
+from collections.abc import Callable
+
+
+'''
+Heuristics
+'''
+def MT(board: Board) -> int:
+    return 
+
+def CB(board: Board) -> int:
+    return 
+
+def NA(board: Board) -> int:
+    return 
+
+
+
+'''
+A* Search 
+'''
+def a_star_search(board: Board, heuristic: Callable[[Board], int]):
+    return
